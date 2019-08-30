12e0807251f19622ae1d3299434b62fbf23c0d26
WBagais <w.bagais@gmail.com>
2019-08-30 18:35:33 -0400

    Edit files

---
+++ helper.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+from collections import defaultdict
+
+df = pd.read_csv('data/survey_results_public.csv')
+schema = pd.read_csv('data/survey_results_schema.csv')
+
+def get_description(column_name, schema=schema):
+    '''
+    INPUT - schema - pandas dataframe with the schema of the developers survey
+            column_name - string - the name of the column you would like to know about
+    OUTPUT -
+            desc - string - the description of the column
+    '''
+    desc = list(schema[schema['Column'] == column_name]['Question'])[0]
+    return desc
+
+#Question 3
+def total_count(df=df, col1, col2, look_for):
+    '''
+    INPUT:
+    df - the pandas dataframe you want to search
+    col1 - the column name you want to look through
+    col2 - the column you want to count values from
+    look_for - a list of strings you want to search for in each row of df[col]
+    OUTPUT:
+    new_df - a dataframe of each look_for with the count of how often it shows up
+    '''
+    new_df = defaultdict(int)
+    #loop through list of ed types
+    for val in look_for:
+        #loop through rows
+        for idx in range(df.shape[0]):
+            #if the ed type is in the row add 1
+            if val in df[col1][idx]:
+                new_df[val] += int(df[col2][idx])
+    new_df = pd.DataFrame(pd.Series(new_df)).reset_index()
+    new_df.columns = [col1, col2]
+    new_df.sort_values('count', ascending=False, inplace=True)
+    return new_df
+
+
+def create_dummy_df(df, cat_cols, dummy_na):
+    '''
+    INPUT:
+    df - pandas dataframe with categorical variables you want to dummy
+    cat_cols - list of strings that are associated with names of the categorical columns
+    dummy_na - Bool holding whether you want to dummy NA vals of categorical columns or not
+
+    OUTPUT:
+    df - a new dataframe that has the following characteristics:
+            1. contains all columns that were not specified as categorical
+            2. removes all the original columns in cat_cols
+            3. dummy columns for each of the categorical columns in cat_cols
+            4. if dummy_na is True - it also contains dummy columns for the NaN values
+            5. Use a prefix of the column name with an underscore (_) for separating
+    '''
+    for col in  cat_cols:
+        try:
+            # for each cat add dummy var, drop original column
+            df = pd.concat([df.drop(col, axis=1), pd.get_dummies(df[col], prefix=col, prefix_sep='_', drop_first=True, dummy_na=dummy_na)], axis=1)
+        except:
+            continue
+    return df
